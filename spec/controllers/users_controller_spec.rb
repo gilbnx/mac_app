7c4f1ebaebf169f028dda15efec18af4b5e928ac
gilbnx <gbgithub1@bennoon.com>
2014-04-25 21:28:40 -0700

    added Gravatar for user show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -19,7 +19,18 @@ describe UsersController do
       assigns(:user).should == @user
     end
 
+    it "should have the right title" do
+      get :show, id: @user
+      response.should have_selector('title', content: @user.name)
+    end
+
+    it "should have a profile image" do
+      get :show, id: @user
+      response.should have_selector('h1>img', class: "gravatar")
+    end
   end
+
+
   describe "GET 'new'" do
     it "returns http success" do
       get :new
